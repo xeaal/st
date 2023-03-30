30dfb004b58c73d352059816fdf44de7af18b37c
xeaal <tryax.use@gmail.com>
2023-03-30 14:30:06 +0500

    configuration slightly changed

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mononoki:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -109,7 +109,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
